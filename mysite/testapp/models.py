18dbda5f08edfe42981e5f551ac150e0ecdc85ca
zyzCodes <diego.earanas1@gmail.com>
2022-06-03 09:21:32 -0600

    second commit

--- mysite/testapp/models.py
+++ mysite/testapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+        
+        
+        
+    # Create your models here:
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    
+    
